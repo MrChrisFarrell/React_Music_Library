54251494c2051bb1e8c6d96e88ca5741c0bac87d
unknown <mrchrisfarrell@yahoo.com>
2021-06-18 15:43:57 -0400

    search bar showing, need to add logic

--- src/Components/MusicTable/musicTable.jsx
+++ src/Components/MusicTable/musicTable.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 
 function MusicTable(props){
     const songRows = props.music.map((song)=>
-    <tr>
-        <td>{song.id}</td>
-        <td>{song.title}</td>
-        <td>{song.artist}</td>
-        <td>{song.album}</td>
-        <td>{song.release_date}</td>
-        <button onClick={()=>props.delete(song.id)}>Delete</button>
-    </tr>
+        <tr>
+            <td>{song.id}</td>
+            <td>{song.title}</td>
+            <td>{song.artist}</td>
+            <td>{song.album}</td>
+            <td>{song.release_date}</td>
+            <button onClick={()=>props.delete(song.id)}>Delete</button>
+        </tr>
     );
     return(
         <div>
-            <table>
+            <input type="text" id="myInput" placeholder="Search for..."/>
+            <table id="myTable">
                 <thead>
                     <tr>Songs</tr>
-                    <tr>
-                        <th>I.D. #</th>
-                        <th>Title</th>
-                        <th>Artist</th>
-                        <th>Album</th>
-                        <th>Release Date</th>
+                    <tr class="header">
+                        <th style={{width:"10%"}}>I.D. #</th>
+                        <th style={{width:"25%"}}>Title</th>
+                        <th style={{width:"25%"}}>Artist</th>
+                        <th style={{width:"25%"}}>Album</th>
+                        <th style={{width:"15%"}}>Release Date</th>
                     </tr>
                 </thead>
-                {songRows}
+                <tbody>{songRows}</tbody>
             </table>
         </div>
     )
